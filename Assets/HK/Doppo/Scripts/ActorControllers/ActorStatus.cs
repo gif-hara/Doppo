a020413778640b7563ee6b994994a2d7d1264e18
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2021-04-18 21:53:41 +0900

    とりあえずActorStatus定義

---
+++ Assets/HK/Doppo/Scripts/ActorControllers/ActorStatus.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Doppo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ActorStatus
+    {
+        public int m_HitPoint;
+    }
+}
