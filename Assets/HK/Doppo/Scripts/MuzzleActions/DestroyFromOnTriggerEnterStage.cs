c168a5ba9b4c0930f56b544223a078404b337e73
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2021-05-23 21:50:29 +0900

    ステージに衝突したら死亡するように

---
+++ Assets/HK/Doppo/Scripts/MuzzleActions/DestroyFromOnTriggerEnterStage.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UniRx;
+using System.Collections.Generic;
+
+namespace HK.Doppo.MuzzleActions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DestroyFromOnTriggerEnterStage : IMuzzleAction
+    {
+        [SerializeField]
+        private int m_Penetrate = default;
+
+        private int m_CurrentPenetrate = 0;
+
+        public void Invoke(Actor spawnedActor, Actor spawnedActorOwner, List<IMuzzleModifier> modifiers, CompositeDisposable disposable)
+        {
+            m_CurrentPenetrate = m_Penetrate;
+            spawnedActor.Events.OnTriggerEnterStageSafe()
+                .Subscribe(_ =>
+                {
+                    m_CurrentPenetrate--;
+                    if (m_CurrentPenetrate <= 0)
+                    {
+                        spawnedActor.Return();
+                    }
+                })
+                .AddTo(disposable);
+        }
+    }
+}
