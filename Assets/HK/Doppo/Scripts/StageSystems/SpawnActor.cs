8a4165795f26c62138a286819adfbd13c087fdb3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2021-05-25 23:59:32 +0900

    Actorを生成するステージイベントを実装

---
+++ Assets/HK/Doppo/Scripts/StageSystems/SpawnActor.cs
@@ -0,0 +1,29 @@
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Doppo.StageSystems.Events
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SpawnActor : IStageTriggerEvent
+    {
+        [SerializeField]
+        private ActorBlueprint m_Blueprint = default;
+
+        [SerializeField]
+        private Transform m_SpawnPoint = default;
+
+        public IObservable<Unit> Invoke()
+        {
+            return Observable.Defer(() =>
+            {
+                m_Blueprint.Spawn(m_SpawnPoint.position, m_SpawnPoint.rotation);
+
+                return Observable.ReturnUnit();
+            });
+        }
+    }
+}
