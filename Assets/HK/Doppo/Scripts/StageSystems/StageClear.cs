3ef2daeb4d3b197cf52404ab892725b881ace1a7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2021-05-26 00:13:13 +0900

    ステージクリアのステージイベント実装

---
+++ Assets/HK/Doppo/Scripts/StageSystems/StageClear.cs
@@ -0,0 +1,23 @@
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Doppo.StageSystems.Events
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class StageClear : IStageTriggerEvent
+    {
+        public IObservable<Unit> Invoke()
+        {
+            return Observable.Defer(() =>
+            {
+                Debug.Log("TODO: Stage Clear");
+
+                return Observable.ReturnUnit();
+            });
+        }
+    }
+}
